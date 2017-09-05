78caa4fb50b8fef4b7076833e2c53e4263fbeda6
Mariusz Nowak <medyk@medikoo.com>
2017-09-05 14:11:36 +0200

    refactor: into prototype based solution
    That should be slightly more efficient, especially when we will want to have level loggers fixed as getters. This however takes us away from well working ES3+ version, but let's forget about it for now

--- index.js
+++ index.js
@@ -3,104 +3,143 @@
 
 "use strict";
 
-var isValue       = require("es5-ext/object/is-value")
-  , objForEach    = require("es5-ext/object/for-each")
-  , assign        = require("es5-ext/object/assign")
-  , ensureString  = require("es5-ext/object/validate-stringifiable-value")
-  , toShortString = require("es5-ext/to-short-string-representation")
-  , ee            = require("event-emitter");
+var assign         = require("es5-ext/object/assign")
+  , setPrototypeOf = require("es5-ext/object/set-prototype-of")
+  , ensureString   = require("es5-ext/object/validate-stringifiable-value")
+  , toShortString  = require("es5-ext/to-short-string-representation")
+  , d              = require("d")
+  , lazy           = require("d/lazy")
+  , ee             = require("event-emitter");
 
 var emitter = ee(), cache = {};
 var isValidLevel = RegExp.prototype.test.bind(/^[a-z]+$/);
 var isValidNsToken = RegExp.prototype.test.bind(/^[a-z0-9-]+$/);
 
-var setChildEnabledState = function (logger, state) {
-	if (isValue(logger._ownIsEnabled)) return;
-	logger.isEnabled = state;
-	objForEach(logger.children, function (childLogger) {
-		setChildEnabledState(childLogger, state);
-	});
+var setEnabledState = function (state) {
+	Object.defineProperty(this, "isEnabled", d("ce", state));
 };
 
-var setEnabledState = function (logger, state) {
-	if (logger._ownIsEnabled === state) return;
-	logger._ownIsEnabled = state;
-	if (logger.isEnabled === state) return;
-	logger.isEnabled = state;
-	objForEach(logger.children, function (childLogger) {
-		setChildEnabledState(childLogger, state);
-	});
+var createLogger, createLevelLogger, createNsLogger;
+
+var getLevel = function (newLevel) {
+	newLevel = ensureString(newLevel);
+	if (!isValidLevel(newLevel)) {
+		throw new TypeError(
+			toShortString(newLevel) + " is not a valid level name (only 'a-z' chars are allowed)"
+		);
+	}
+	if (this.level === newLevel) return this;
+	var levelLogger = createLevelLogger(newLevel);
+	return this.nsTokens.reduce(function (currentLogger, token) {
+		return createNsLogger(currentLogger, token);
+	}, levelLogger);
 };
 
-var getLogger;
-var getNsLogger = function (logger, nsToken) {
-	if (logger.children[nsToken]) return logger.children[nsToken];
-	return getLogger(logger.level, logger, nsToken);
-};
-
-getLogger = function (level, parent, nsToken) {
-	if (!parent && cache[level]) return cache[level];
-	var logger = assign(
-		// eslint-disable-next-line no-unused-vars
-		function (msgItem1 /*, ...msgItemn*/) {
-			if (!logger.isEnabled) return;
-			var event = {
-				logger: logger,
-				date: new Date(),
-				messageTokens: arguments
-			};
-			emitter.emit("log:before", event);
-			emitter.emit("log", event);
-		},
-		{
-			level: level,
-			ns: parent && parent.ns ? parent.ns + ":" + nsToken : nsToken || null,
-			nsTokens: nsToken ? parent.nsTokens.concat(nsToken) : [],
-			isEnabled: parent ? parent.isEnabled : true,
-			emitter: emitter,
-			children: [],
-			enable: function () {
-				setEnabledState(logger, true);
-			},
-			disable: function () {
-				setEnabledState(logger, false);
-			},
-			getLevel: function (newLevel) {
-				newLevel = ensureString(newLevel);
-				if (!isValidLevel(newLevel)) {
-					throw new TypeError(
-						toShortString(newLevel) +
-							" is not a valid level name (only 'a-z' chars are allowed)"
-					);
-				}
-				if (level === newLevel) return logger;
-				var levelLogger = getLogger(newLevel);
-				return logger.nsTokens.reduce(function (currentLogger, token) {
-					return getNsLogger(currentLogger, token);
-				}, levelLogger);
-			},
-			getNs: function (newNs) {
-				newNs = ensureString(newNs);
-				var newNsTokens = newNs.split(":");
-				newNsTokens.forEach(function (newNsToken) {
-					if (!isValidNsToken(newNsToken)) {
-						throw new TypeError(
-							toShortString(newNs) +
-								" is not a valid ns string " +
-								"(only 'a-z0-9-' chars are allowed and ':' as delimiter)"
-						);
-					}
-				});
-				return newNsTokens.reduce(function (currentLogger, token) {
-					return getNsLogger(currentLogger, token);
-				}, logger);
-			}
+var getNs = function (newNs) {
+	newNs = ensureString(newNs);
+	var newNsTokens = newNs.split(":");
+	newNsTokens.forEach(function (newNsToken) {
+		if (!isValidNsToken(newNsToken)) {
+			throw new TypeError(
+				toShortString(newNs) +
+					" is not a valid ns string " +
+					"(only 'a-z0-9-' chars are allowed and ':' as delimiter)"
+			);
 		}
-	);
-	if (parent) parent.children[nsToken] = logger;
-	else cache[level] = logger;
+	});
+	return newNsTokens.reduce(function (currentLogger, token) {
+		return createNsLogger(currentLogger, token);
+	}, this);
+};
+
+var loggerProto = Object.create(
+	Function.prototype,
+	assign(
+		{ isEnabled: d("e", true), emitter: d("", emitter), _nsToken: d("", null) },
+		lazy({
+			ns: d(
+				"e",
+				function () {
+					return this.nsTokens.join(":") || null;
+				},
+				{ cacheName: "_ns" }
+			),
+			nsTokens: d(
+				"e",
+				function () {
+					return this._nsToken
+						? Object.getPrototypeOf(this).nsTokens.concat(this._nsToken)
+						: [];
+				},
+				{ cacheName: "_nsTokens" }
+			),
+			children: d(
+				"",
+				function () {
+					return {};
+				},
+				{ cacheName: "_children" }
+			),
+			enable: d(
+				function () {
+					return setEnabledState.bind(this, true);
+				},
+				{ cacheName: "_enable" }
+			),
+			disable: d(
+				function () {
+					return setEnabledState.bind(this, false);
+				},
+				{ cacheName: "_disable" }
+			),
+			getLevel: d(
+				function () {
+					return getLevel.bind(this);
+				},
+				{ cacheName: "_getLevel" }
+			),
+			getNs: d(
+				function () {
+					return getNs.bind(this);
+				},
+				{ cacheName: "_getNs" }
+			)
+		})
+	)
+);
+
+createLogger = function () {
+	// eslint-disable-next-line no-unused-vars
+	return function self (msgItem1 /*, ...msgItemn*/) {
+		if (!self.isEnabled) return;
+		var event = {
+			logger: self,
+			date: new Date(),
+			messageTokens: arguments
+		};
+		emitter.emit("log:before", event);
+		emitter.emit("log", event);
+	};
+};
+
+createLevelLogger = function (level) {
+	if (cache[level]) return cache[level];
+	var logger = Object.defineProperties(setPrototypeOf(createLogger(), loggerProto), {
+		level: d("e", level)
+	});
+	cache[level] = logger;
 	emitter.emit("init", { logger: logger });
 	return logger;
 };
 
-module.exports = getLogger("debug");
+createNsLogger = function (parent, nsToken) {
+	if (parent.children[nsToken]) return parent.children[nsToken];
+	var logger = Object.defineProperties(setPrototypeOf(createLogger(), parent), {
+		_nsToken: d("", nsToken)
+	});
+	parent.children[nsToken] = logger;
+	emitter.emit("init", { logger: logger });
+	return logger;
+};
+
+module.exports = createLevelLogger("debug");
