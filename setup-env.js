a873045da109accbb32b7564fb8a0a3477053f95
Mariusz Nowak <medyk@medikoo.com>
2017-09-06 12:55:10 +0200

    feat: setupEnv functionality

---
+++ setup-env.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var aFrom        = require("es5-ext/array/from")
+  , isValue      = require("es5-ext/object/is-value")
+  , ensureObject = require("es5-ext/object/valid-object")
+  , objMap       = require("es5-ext/object/map")
+  , ensureString = require("es5-ext/object/validate-stringifiable-value")
+  , logger       = require("./");
+
+// Conf should be map of namespace tokens per level, e.g.:
+// { debug: "this,-not-this", info: "*" }
+module.exports = function (conf) {
+	conf = objMap(ensureObject(conf), function (levelConf, level) {
+		var map = Object.create(null);
+		var levelLogger;
+		if (logger.hasLevel(level)) levelLogger = logger.getLevel(level);
+		aFrom(levelConf).forEach(function (ns) {
+			ns = ensureString(ns);
+			var isEnabled = ns[0] !== "-";
+			if (!isEnabled) ns = ns.slice(1);
+			map[ns] = isEnabled;
+			if (!levelLogger) return;
+			var targetLogger;
+			if (ns === "*") targetLogger = levelLogger;
+			else if (levelLogger.hasNs(ns)) targetLogger = levelLogger.getNs(ns);
+			if (!targetLogger) return;
+			targetLogger[isEnabled ? "enable" : "disable"]();
+		});
+		return map;
+	});
+
+	logger.emitter.on("init", function (event) {
+		var newLogger = event.logger;
+		var levelConf = conf[newLogger.level];
+		if (!levelConf) return;
+		var isEnabled = levelConf[newLogger.ns || "*"];
+		if (isValue(isEnabled)) newLogger[isEnabled ? "enable" : "disable"]();
+	});
+};
