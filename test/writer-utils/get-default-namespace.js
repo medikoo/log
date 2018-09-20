3c99624f8403e318864022e91a06710318b24b28
Mariusz Nowak <medyk@medikoo.com>
2018-09-20 17:22:09 +0200

    feat: support default namespace concept
    It's for writers which may omit namespace token when dealing with
    default namespace

---
+++ test/writer-utils/get-default-namespace.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var test            = require("tape")
+  , requireUncached = require("cjs-module/require-uncached");
+
+test("Register master", function (t) {
+	var getDefaultNamespace = requireUncached(
+		require.resolve("../../writer-utils/get-default-namespace"),
+		function () { return require("../../writer-utils/get-default-namespace"); }
+	);
+
+	t.test("Preset default namespace is null", function (t) {
+		t.equal(getDefaultNamespace(), null);
+		t.end();
+	});
+
+	t.test("Default namespace should be setable via internal _set method", function (t) {
+		getDefaultNamespace._set("foo");
+		t.equal(getDefaultNamespace(), "foo");
+		t.end();
+	});
+	t.end();
+});
