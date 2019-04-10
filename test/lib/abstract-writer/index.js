42e566529cfbdb9e54ed65df0d74e0704711c8c7
Mariusz Nowak <medyk@medikoo.com>
2019-04-10 13:00:05 +0200

    test: make room for multiple test files

--- test/lib/abstract-writer/index.js
+++ test/lib/abstract-writer/index.js
@@ -6,17 +6,18 @@ var test            = require("tape")
 var resolveUncached = function () {
 	return requireUncached(
 		[
-			require.resolve("../../"), require.resolve("../../lib/abstract-writer"),
-			require.resolve("../../lib/private/logger-prototype"),
-			require.resolve("../../lib/private/logger-prototype/namespace-props"),
-			require.resolve("../../lib/emitter"), require.resolve("../../lib/get-master-writer"),
-			require.resolve("../../lib/setup-visibility")
+			require.resolve("../../../"), require.resolve("../../../lib/abstract-writer"),
+			require.resolve("../../../lib/private/logger-prototype"),
+			require.resolve("../../../lib/private/logger-prototype/namespace-props"),
+			require.resolve("../../../lib/emitter"),
+			require.resolve("../../../lib/get-master-writer"),
+			require.resolve("../../../lib/setup-visibility")
 		],
 		function () {
 			return {
-				log: require("../../"),
-				LogWriter: require("../../lib/abstract-writer"),
-				emitter: require("../../lib/emitter")
+				log: require("../../.."),
+				LogWriter: require("../../../lib/abstract-writer"),
+				emitter: require("../../../lib/emitter")
 			};
 		}
 	);
