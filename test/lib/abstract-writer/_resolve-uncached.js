3bbb7cd1779afdb634a9fcab45681bbe4cac140f
Mariusz Nowak <medyk@medikoo.com>
2019-04-10 14:01:14 +0200

    test: fix require paths

--- test/lib/abstract-writer/_resolve-uncached.js
+++ test/lib/abstract-writer/_resolve-uncached.js
@@ -5,17 +5,18 @@ var requireUncached = require("ncjsm/require-uncached");
 module.exports = function () {
 	return requireUncached(
 		[
-			require.resolve("../../"), require.resolve("../../lib/abstract-writer"),
-			require.resolve("../../lib/private/logger-prototype"),
-			require.resolve("../../lib/private/logger-prototype/namespace-props"),
-			require.resolve("../../lib/emitter"), require.resolve("../../lib/get-master-writer"),
-			require.resolve("../../lib/setup-visibility")
+			require.resolve("../../../"), require.resolve("../../../lib/abstract-writer"),
+			require.resolve("../../../lib/private/logger-prototype"),
+			require.resolve("../../../lib/private/logger-prototype/namespace-props"),
+			require.resolve("../../../lib/emitter"),
+			require.resolve("../../../lib/get-master-writer"),
+			require.resolve("../../../lib/setup-visibility")
 		],
 		function () {
 			return {
-				log: require("../../"),
-				LogWriter: require("../abstract-writer"),
-				emitter: require("../emitter")
+				log: require("../../../"),
+				LogWriter: require("../../../lib/abstract-writer"),
+				emitter: require("../../../lib/emitter")
 			};
 		}
 	);
