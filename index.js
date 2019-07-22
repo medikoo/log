81ac0cb37012699dbeaffc50c84437e91859855e
Oliver Hattshire <Hattshire@users.noreply.github.com>
2019-07-22 12:11:22 +0000

    docs: Fix typo
    Misleading outdated comment.

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 
 var loggerPrototype = require("./lib/private/logger-prototype");
 
-// Exports "debug" level logger as a starting point
+// Exports "info" level logger as a starting point
 module.exports = loggerPrototype._createLevel("info");
